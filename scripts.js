c63b46df830cf2386c3b704c287da175d8cc6cdc
Henrique <henriquecamilomapa@gmail.com>
2020-02-17 22:05:01 -0300

    criação novo js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelector('header button').addEventListener("click", function() {
+
+    document.querySelector('.form').classList.toggle('hide') //add a classe hide ao .form
+    //dessa forma existe um css que vai transformar a classe hide em display none
+
+})
